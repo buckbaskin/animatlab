ffb2e683ac214aeec36a830f97f5f45dcba2cca5
Buck Baskin <mobile.wbaskin@gmail.com>
2018-03-07 16:43:51 -0500

    It's not quite ready, need different torques for left and right

--- docs/TODO.md
+++ docs/TODO.md
@@ -4,7 +4,7 @@
 
 ### Next Steps
 - [x] Linear stability analysis of PD angle/torque control for large angles
-- [ ] Level curve for stiffness -> pick stiffness -> select angle from there
+- [.] Level curve for stiffness -> pick stiffness -> select angle from there
 - [ ] Build a dynamic model, 2 different Festos, negative feedback based on pressure/position curves
 - [ ] Incorporate a discrete pressure update, update frequency of the controllers. Limits stability at high stiffness
 - [ ] Then do lagging mass models that show how low stiffness can get
--- stability/plot_level_curves.py
+++ stability/plot_level_curves.py
@@ -20,14 +20,48 @@ import numpy as np
 fig = plt.figure()
 ax = fig.gca(projection='3d')
 
-max_pressure = 5
-min_pressure = -5
+### Search Parameters ###
+
+max_torque = 2
+min_torque = -2
+max_theta = pi / 2
+min_theta = -pi / 2
 resolution = 0.1
 
+### Model Parameters ###
+
+a0 = 254.3 # kpa
+a1 = 192.0 # kpa
+a2 = 2.0625
+a3 = -0.461
+a4 = -0.331
+a5 = 1.230
+a6 = 15.6 # kpa
+
+### Mutual Actuator Parameters ### 
+
+l_rest = .189 # m
+l_620 = round(-((.16 * l_rest) - l_rest), 3)
+k_max = round((l_rest - l_620) / (l_rest), 3)
+
+### Specific Actuator Parameters ###
+
+d_l = 1
+alpha_l = 0
+beta_l = 0
+l0_l = 1
+l1_l = 1
+
+d_r = 1
+alpha_r = 0
+beta_r = 0
+l0_r = 1
+l1_r = 1
+
 # Make data.
-X = np.arange(min_pressure, max_pressure, resolution)
-Y = np.arange(min_pressure, max_pressure, resolution)
-X, Y = np.meshgrid(X, Y)
+T = np.arange(min_torque, max_torque, resolution)
+A = np.arange(min_theta, max_theta, resolution)
+T, A = np.meshgrid(T, A)
 
 ### calculate the torque here
 # for any two pressures w/in reason
@@ -45,8 +79,11 @@ X, Y = np.meshgrid(X, Y)
 # the mirroed actuators. Write down the two pressures in Xp, Yp, and two values
 # Z_torque, Z_angle. Plot XYZ surface for both at different stiffness
 
-R = np.sqrt(X**2 + Y**2)
-Z = np.sin(R)
+L_angle_l = l0_l + l1_l * np.cos(alpha_l + A)
+F_l = T / (d_l * np.cos(beta_l + A))
+K_l = (l_rest - L_angle_l) / l_rest
+
+P_l = a0 + a1 * np.tan(a2 * (K_l / (a4 * F_l + k_max)) + a3) + a5 * F_l # kpa
 
 # Plot the surface.
 surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,
