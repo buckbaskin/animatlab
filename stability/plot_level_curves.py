b2b841b6077ea70bafa878f40f0ace1a40fafdb7
Buck Baskin <mobile.wbaskin@gmail.com>
2018-03-08 00:24:09 -0500

    Something about the pressure model is off, because always 620 kpa

--- stability/plot_level_curves.py
+++ stability/plot_level_curves.py
@@ -17,14 +17,16 @@ from matplotlib.ticker import LinearLocator, FormatStrFormatter
 import numpy as np
 
 
+
+
 ### Search Parameters ###
 
 max_torque = 2.001
 min_torque = -2
-torque_resolution = 1
+torque_resolution = 0.5
 max_theta = pi / 4 + .001
 min_theta = -pi / 4
-angle_resolution = pi / 8.00
+angle_resolution = pi / 8.0
 
 ### Model Parameters ###
 
@@ -54,8 +56,11 @@ l0 = floor((l_max - l1) * 1000.0) / 1000.0
 MAX_STIFFNESS = 2
 MIN_STIFFNESS = 0
 
-MAX_FORCE = 15 * 9.81 # Newtons, about 15 kg
-MIN_FORCE = -15 * 9.81 # N
+FORCE_MAX = 15 * 9.81 # Newtons, about 15 kg
+FORCE_MIN = -15 * 9.81 # N
+
+PRESSURE_MAX = 620
+PRESSURE_MIN = 0
 
 ### Specific Actuator Parameters ###
 # Actuator L is the negative actuator, Actuator R is the positive actuator
@@ -75,19 +80,34 @@ beta_l = -pi / 2 # radians, TODO(buckbaskin): assumes that muscle mounted d mete
 alpha_r = -atan(offset / d) # radians
 beta_r = pi / 2 # radians, TODO(buckbaskin): assumes that muscle mounted d meters off mount
 
-# Make data
-stiffness = 1 # N-m (torque)
 
-stiffness = max(0, min(2, stiffness)) # force boundary limits
+def pressure(T, A):
+    F = T / (d * np.cos(beta_l + A))
+    L_angle = l0 + l1 * np.cos(alpha_l + A)
+    K = (l_rest - L_angle) / l_rest
+    P = a0 + a1 * np.tan(a2 * (K / (a4 * F + k_max)) + a3) + a5 * F # kpa
+    return np.clip(P, PRESSURE_MIN, PRESSURE_MAX)
+
+fun = pressure(1, pi/8)
+
+for i in np.arange(-pi/2, pi/2+.001, .01):
+    state_t = 1
+    state_a = pi/8
+    dPdA = (pressure(state_t, state_a+0.01) - pressure(state_t, state_a-0.01)) / (0.01 - (-0.01))
+    dPdT = (pressure(state_t+0.01, state_a) - pressure(state_t-0.01, state_a)) / (0.01 - (-0.01))
+    print(pressure(state_t, state_a))
+    print('dPdA', dPdA)
+    print('dPdT', dPdT)
+1/0
+
+
+### Make data ###
+stiffness = 1 # N-m (torque)
+stiffness = np.clip(stiffness, 0, 2) # force boundary limits
 
 T = np.arange(min_torque, max_torque, torque_resolution) # N-m, net torque
 T_r = (0.5 * T) + stiffness
 T_l = (0.5 * T) - stiffness
-
-print('Torques (des, r, l)')
-print(T)
-print(T_r)
-print(T_l)
 
 A = np.arange(min_theta, max_theta, angle_resolution) # radians, angle1
 A2 = A.copy()
@@ -104,23 +124,19 @@ L_angle_l = l0 + l1 * np.cos(alpha_l + A)
 L_angle_r = l0 + l1 * np.cos(alpha_r + A)
 F_l = T_l / (d * np.cos(beta_l + A))
 F_r = T_r / (d * np.cos(beta_r + A))
-
-# F_l = np.threshold, np.trim?
-# F_r = np.trim(F_r, MIN_FORCE, MAX_FORCE)
-
-print('T_l')
-print(T_l)
-print('A')
-print(A)
-print('F_l')
-print(F_l) # , F_r)
-fail = 1/0
+np.clip(F_l, FORCE_MIN, FORCE_MAX, out=F_l)
+np.clip(F_r, FORCE_MIN, FORCE_MAX, out=F_r)
 
 K_l = (l_rest - L_angle_l) / l_rest
 K_r = (l_rest - L_angle_l) / l_rest
 
 P_l = a0 + a1 * np.tan(a2 * (K_l / (a4 * F_l + k_max)) + a3) + a5 * F_l # kpa
 P_r = a0 + a1 * np.tan(a2 * (K_r / (a4 * F_r + k_max)) + a3) + a5 * F_r # kpa
+np.clip(P_l, PRESSURE_MIN, PRESSURE_MAX, out=P_l)
+np.clip(P_r, PRESSURE_MIN, PRESSURE_MAX, out=P_r)
+
+print(P_r)
+1/0
 
 # Plot the surface.
 fig = plt.figure()
