dae30e2f7ffe7c05ab2d7ea50bed65ac38e7374d
Buck Baskin <mobile.wbaskin@gmail.com>
2018-04-03 17:16:54 -0400

    Visualize the pressure torque relation in its own file

---
+++ stability/pressure_torque.py
@@ -0,0 +1,57 @@
+import sys
+print('--- %s ---' % (sys.argv[0],))
+
+import datetime
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+
+from functools import partial
+from math import pi
+from numpy import arctan, sqrt, floor, ceil
+from simple_mass_model import ActualSimulator, SimpleSimulator
+from simple_mass_model import OptimizingController
+
+ERROR_STANDARD = 1 # degree
+ERROR_STANDARD = ERROR_STANDARD / 180 * pi # radians
+SCORING_DELAY = 750
+
+class FrozenOptimizingController(OptimizingController):
+    def control(self, state, desired_states, times):
+        self.est_state = self.sensor_fusion(self.est_state, self.last_est_time,
+            self.lag_pos, state, times[0])
+
+        self.last_est_time = times[0]
+        self.lag_pos = self.est_state[0]
+
+        des_torque = self._pick_torque(self.est_state, desired_states, times)
+        des_ext_pres, des_flx_pres = self._convert_to_pressure(des_torque, state)
+
+        self.last_control = des_torque
+
+        return des_ext_pres, des_flx_pres, des_torque
+
+if __name__ == '__main__':
+    S = ActualSimulator(bang_bang=True, limit_pressure=True, TIME_END = 2.0)
+    
+    plt.title('Pressure Torque Relation (Flx)')
+    angles = np.linspace(-math.pi * 1 / 4, math.pi * 1 / 4 - 0.001, 5)
+    for angle in angles:
+        state = np.zeros((5,))
+        state[0] = angle
+        torques = np.linspace(0.01, 3, 400)
+        pressures = S.flx_torque_to_pressure(torques, state) # * np.cos(angle)
+        # this shows that the entire thing can be calculated by a normalized 
+        # linear thing, then divided by cosine(theta)
+        #   Input range: 0 -> 2.5 Nm <> 0 to 20 mV
+        #   Output range: 0 -> 620 Nm <> 0 to 20 mV
+        # But then divide by cos(angle) -> increasing torque requirements for
+        #   increasing angle
+        # This doesn't quite correspond with how the neurons do
+        plt.plot(torques, pressures, label='%.3f pi (rad)' % (angle / math.pi,))
+    plt.legend()
+    plt.ylabel('Pressure (kPa)')
+    plt.xlabel('Torque (Nm)')
+    print('go find the plot and close it please')
+    plt.show()
+    print('all done for real now')
