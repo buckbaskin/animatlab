6403cfe3ff2aee57eb98174af35781431c9c0828
Buck Baskin <mobile.wbaskin@gmail.com>
2018-04-24 23:35:47 -0400

    Add new references to the bibliography

--- writeup/compile.bash
+++ writeup/compile.bash
@@ -1,3 +1,3 @@
-biber --input-directory out --output-directory out main
 pdflatex -output-directory=out main.tex
+biber --input-directory out --output-directory out main
 pdflatex -output-directory=out main.tex
