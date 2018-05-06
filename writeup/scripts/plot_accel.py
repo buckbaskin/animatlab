53bb1af2c76a133af5591bc9b97926ee81df96f0
Buck Baskin <mobile.wbaskin@gmail.com>
2018-05-06 17:29:52 -0400

    Plot Accels with Ref

--- writeup/scripts/plot_accel.py
+++ writeup/scripts/plot_accel.py
@@ -15,10 +15,30 @@ pos_net = data[:, 5]
 neg_net = data[:, 6]
 
 # TODO(buckbaskin): define reference accelerations for positive, negative
+# 5000 steps per second
+pos_ref = np.zeros(pos_net.shape)
+pos_ref[5000:10000] = 19.7
+pos_ref[15000:20000] = 9.9
+pos_ref[25000:30000] = 18.5
+pos_ref[35000:40000] = 9.2
+pos_ref[45000:50000] = 18.1
+pos_ref[55000:60000] = 9.1
+
+pos_ref += -60
+
+neg_ref = np.zeros(neg_net.shape)
+neg_ref[10000:15000] = 19.7
+neg_ref[20000:25000] = 9.9
+neg_ref[30000:35000] = 18.5
+neg_ref[40000:45000] = 9.2
+neg_ref[50000:55000] = 18.1
+neg_ref[60000:65000] = 9.1
+
+neg_ref += -60
 
 setup = [
-('Pos', [pos_net,]),
-('Neg', [neg_net,]),
+('Pos', [pos_net, pos_ref]),
+('Neg', [neg_net, neg_ref]),
 ]
 
 linewidth = 2
@@ -27,10 +47,10 @@ for name, datasets in setup:
     fig = plt.figure(figsize=(6,3,), dpi=300)
     ax = fig.add_subplot('111')
 
-    vel, = datasets
+    vel, ref = datasets
     # print(len(time))
-    ax.plot(time[200:], vel[200:], linewidth=linewidth, label='Est. Acc.')
-    # ax.plot(time[20000:40000], ref[20000:40000], linewidth=linewidth, label='Reference')
+    ax.plot(time[5000:], vel[5000:], linewidth=linewidth, label='Est. Acc.')
+    ax.plot(time[5000:], ref[5000:], linewidth=linewidth, label='Reference')
     
     ax.set_xlabel('Time (sec)')
     ax.set_ylabel('Acceleration (mV)')
